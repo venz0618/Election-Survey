acf0edad4da9d249036c2305b427e26859a223cf
venz0618 <100183442+venz0618@users.noreply.github.com>
2025-04-03 14:13:50 +0800

    Update axiosInstance.jsx

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:8000/api",    //http://127.0.0.1:8000
+    baseURL: "http://localhost:8000/api",    //http://127.0.0.1:8000
 });
 
 // Attach token automatically
