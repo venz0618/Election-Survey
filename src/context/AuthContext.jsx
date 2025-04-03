3a1404ecea0c253afd98b3e45b45ffc376131b69
venz0618 <100183442+venz0618@users.noreply.github.com>
2025-04-03 14:34:52 +0800

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -87,7 +87,7 @@ export const AuthProvider = ({ children }) => {
 
     const login = async (email, password) => {
         try {
-            const response = await fetch("http://localhost:8000/api/login", {    
+            const response = await fetch("https://6ca5-143-44-192-49.ngrok-free.app/api/login", {    
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, password }),
