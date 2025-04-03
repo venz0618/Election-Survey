1386ae1db1160f7b6a8173ad8c3ae0beeaed94d0
venz0618 <100183442+venz0618@users.noreply.github.com>
2025-04-03 14:35:09 +0800

    Update axiosInstance.jsx

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const axiosInstance = axios.create({
-    baseURL: "http://https://6ca5-143-44-192-49.ngrok-free.app//api",    //http://127.0.0.1:8000
+    baseURL: "http://https://6ca5-143-44-192-49.ngrok-free.app/api",    //http://127.0.0.1:8000
 });
 
 // Attach token automatically
